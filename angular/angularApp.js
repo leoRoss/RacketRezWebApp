f92268e7ed995f13516459083f35ffa61b7a28f4
Matt Hooper <mtthpr@gmail.com>
2014-10-13 11:29:58 -0400

    Add gitignore and add datetimepicker

--- angular/angularApp.js
+++ angular/angularApp.js
@@ -1,10 +1,13 @@
 (function() {
+
   var app = angular.module('racketRez', []);
 
   app.controller('ReservationController', ['$http', function($http) {
     var res = this;
 
     res.reservations = [];
+
+    // cheap way to mock data
     $http.get('reservations.json').success(function(data) {
       res.reservations = data;
     });
